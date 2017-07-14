843bad25854ce6c8a83f3bbd682dc0b0c55f3f73
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-07-14 19:09:50 +0100

    Fixes sed to cope with unescaped path separators.

--- test-runner.sh
+++ test-runner.sh
@@ -7,7 +7,7 @@ PYTHONPATH=. coverage run -m unittest discover -v -s gitcommonsync/tests
 coverage run setup.py install
 
 # Awful bit of munging to map coverage to the copy of the module in the project package
-sed -i -e "s/[^\"]*ansible_module.py/${PROJECT_ROOT}\/gitcommonsync\/ansible_module.py/g" .coverage*
+sed -i -e "s#[^\"]*ansible_module.py#${PROJECT_ROOT}\/gitcommonsync\/ansible_module.py#g" .coverage*
 
 coverage combine -a
 coverage report
