9f0b8d76921d073252b46664b909e62c8f69c94e
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-07-14 15:56:47 +0100

    Performs some magic to get test coverage of the Ansible module.

--- gitcommonsync/tests/ansible/library/gitcommonsync.py
+++ gitcommonsync/tests/ansible/library/gitcommonsync.py
@@ -1,1 +1,26 @@
-../../../ansible_module.py
+# XXX: I have not worked it out but it appears that this module gets loaded correctly if there is some padding at the
+# top!? It is not obvious that the Ansible import is required but I have added it for good measure. Expect issues
+# using this module in the future...
+# noinspection PyUnresolvedReferences
+from ansible.module_utils.basic import AnsibleModule
+import os
+from coverage import Coverage
+from distutils.sysconfig import get_python_lib
+
+_PROJECT_ROOT = os.environ["PROJECT_ROOT"]
+
+
+def main():
+    coverage = Coverage(data_file=f"{_PROJECT_ROOT}/.coverage.ansible", branch=True,
+                        include=f"{get_python_lib()}/gitcommonsync*/gitcommonsync/ansible_module.py")
+    coverage.start()
+    try:
+        from gitcommonsync.ansible_module import main as real_main
+        real_main()
+    finally:
+        coverage.stop()
+        coverage.save()
+
+
+if __name__ == "__main__":
+    main()
--- test-runner.sh
+++ test-runner.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+export PROJECT_ROOT=/data
+
 PYTHONPATH=. coverage run -m unittest discover -v -s gitcommonsync/tests
+
 coverage run setup.py install
+
+# Awful bit of munging to map coverage to the copy of the module in the project package
+sed -i -e 's/[^"]*ansible_module.py/\/data\/gitcommonsync\/ansible_module.py/g' .coverage*
+
 coverage combine -a
 coverage report
