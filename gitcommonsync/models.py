a0e770a01f2c3c8aa481ad69ecb91d085caeb8fd
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-07-18 15:15:46 +0100

    Removed unused code.

--- gitcommonsync/models.py
+++ gitcommonsync/models.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta
-from typing import List, Dict
+from typing import Dict
 
 from gitcommonsync._git import GitCheckout
 
@@ -45,22 +45,3 @@ class TemplateSynchronisation(FileSynchronisation):
     def __init__(self, source: str, destination: str, variables: Dict[str, str], overwrite: bool=False):
         super().__init__(source, destination, overwrite=overwrite)
         self.variables = variables
-
-#
-# class MultiSynchronisation(Synchronisation):
-#     """
-#     All synchronisation configurations.
-#     """
-#     def __init__(self, files: List[FileSynchronisation]=None, subrepos: List[SubrepoSynchronisation]=None,
-#                  templates: List[TemplateSynchronisation]=None):
-#         self.files = files if files is not None else []
-#         self.subrepos = subrepos if subrepos is not None else []
-#         self.templates = templates if templates is not None else []
-#
-#     def get_number_of_synchronisations(self) -> int:
-#         """
-#         TODO
-#         :return:
-#         """
-#         return len(self.files) + len(self.subrepos) + len(self.templates)
-#
