36c776006c79f9b48ac63e4044a4dcacf47c87c9
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-02-06 12:36:23 +0000

    Minor change to test runner.

--- test-runner.sh
+++ test-runner.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-export PYTHON_COVERAGE=1
-PYTHONPATH=. coverage run -m unittest discover -v -s gitcommonsync/tests
+PYTHON_COVERAGE=1 PYTHONPATH=. coverage run -m unittest discover -v -s gitcommonsync/tests
 
 coverage run setup.py install
 
